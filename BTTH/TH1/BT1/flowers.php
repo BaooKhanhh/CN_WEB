467c528381d4adc3dd691e9b97d04741a98b71ec
BaooKhanhh <khanhdomino216@gmail.com>
2024-11-28 21:30:54 +0700

    update

--- BTTH/TH1/BT1/flowers.php
+++ BTTH/TH1/BT1/flowers.php
@@ -2,27 +2,33 @@
 $flowers = array (
   0 => 
   array (
-    'name' => 'Hoa hướng dương',
-    'description' => 'biểu tượng của mặt trời',
-    'image' => 'images/hoa2.jpg',
+    'name' => 'Hoa đỗ quyên',
+    'description' => 'Đẹp',
+    'image' => 'images/doquyen.jpg',
   ),
   1 => 
   array (
-    'name' => 'Hoa sen',
-    'description' => 'đẹp',
-    'image' => 'images/hoa8.webp',
+    'name' => 'Hoa Mai',
+    'description' => 'Rất đẹp và xuất hiện vào dịp tết',
+    'image' => 'images/mai.jpg',
   ),
   2 => 
   array (
-    'name' => 'hoa cẩm trướng',
-    'description' => 'đẹp ác',
-    'image' => 'images/hoa7.webp',
+    'name' => 'Hoa tường vy',
+    'description' => 'Đẹp xuất sắc',
+    'image' => 'images/tuongvy.jpg',
   ),
   3 => 
   array (
-    'name' => 'hoa giấy',
-    'description' => 'bình thường',
-    'image' => 'images/hoa4.webp',
+    'name' => 'Hoa hải đường',
+    'description' => 'Pam yeu oi',
+    'image' => 'images/haiduong.jpg',
+  ),
+  4 => 
+  array (
+    'name' => 'Hoa hồng',
+    'description' => 'Đẹp và có gai',
+    'image' => 'images/cach-trong-hoa-hong-tu-canh-01.jpg',
   ),
 );
 ?>
